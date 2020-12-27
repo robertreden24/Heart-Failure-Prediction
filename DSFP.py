1d9e1187c3b42b6001f449f149fc860d8552318e
Robert Reden <43334133+robertreden24@users.noreply.github.com>
2020-12-27 16:09:45 +0700

    Update DSFP.py

--- DSFP.py
+++ DSFP.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+
 
 
 df = pd.read_csv('hfdataset.csv')
@@ -25,6 +27,7 @@ def corr_matrix(df):
 
 # Labels are the values we want to predict
 labels = np.array(df['DEATH_EVENT'])
+
 # Remove the labels from the features
 # axis 1 refers to the columns
 df= df.drop('DEATH_EVENT', axis = 1)
@@ -41,6 +44,25 @@ print('Training Labels Shape:', train_labels.shape)
 print('Testing Features Shape:', test_features.shape)
 print('Testing Labels Shape:', test_labels.shape)
 
+# The baseline predictions are the historical averages
+baseline_preds = test_features[:, column_list.index('serum_creatinine')]
+
+# Baseline errors, and display average baseline error
+baseline_errors = abs(baseline_preds - test_labels)
+print('Average baseline error: ', round(np.mean(baseline_errors), 2))
+
+# Instantiate model with 1000 decision trees
+rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
+# Train the model on training data
+rf.fit(train_features, train_labels);
+
+# Use the forest's predict method on the test data
+predictions = rf.predict(test_features)
+# Calculate the absolute errors
+errors = abs(predictions - test_labels)
+# Print out the mean absolute error (mae)
+print('Mean Absolute Error:', round(np.mean(errors), 2), 'degrees.')
+print('Errors:', errors)
 
 
 
